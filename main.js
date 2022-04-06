36ef17381ef957cd30e49efb1aefe860d593de5d
NVHViet99 <hoangviet220699@gmail.com>
2022-04-06 11:00:57 +0700

    fix fonts and adjust icons

--- main.js
+++ main.js
@@ -1,30 +1,18 @@
 const openBtn = document.querySelector('.menu-bar');
-const openBtnInside = document.querySelector('.menu-bar-inside');
 const showMenu = document.querySelector('.nav-menu');
-const content = document.querySelector('.content');
-const sideBar = document.querySelector('.container');
-const navContent = document.querySelectorAll('.nav-content');
-const navBar = document.querySelector('.nav-bar');
 
 function hasClass(element, cls) {
   return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
-document.querySelector('.content').style.marginLeft = '5rem';
+
+document.querySelector('.content').style.marginLeft = '4.5rem';
 
 openBtn.addEventListener('click', function () {
   if (hasClass(showMenu, 'hide-menu')) {
     showMenu.classList.remove('hide-menu');
-    document.querySelector('.content').style.marginLeft = '15rem';
+    document.querySelector('.content').style.marginLeft = '17rem';
   } else {
     showMenu.classList.add('hide-menu');
-    document.querySelector('.content').style.marginLeft = '5rem';
+    document.querySelector('.content').style.marginLeft = '4.5rem';
   }
 });
-// const closeSieBar = function () {
-//   // sideBar.classList.remove('overlay');
-//   document.querySelector('.content').style.marginLeft = '23px';
-//   navBar.classList.remove('hide');
-//   showMenu.classList.add('hide-menu');
-// };
-
-// openBtnInside.addEventListener('click', closeSieBar);
