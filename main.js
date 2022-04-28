aa86e45c66fa09817a7ceff57257e7f6a01d4df0
NVHViet99 <hoangviet220699@gmail.com>
2022-04-28 23:01:33 +0700

    add chip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const navBtn = document.querySelector(".hamburger-menu");
 const showMenu = document.querySelector(".nav-menu");
 const body = document.querySelector("body");
+const chip = document.querySelector(".chip");
 
 function hasClass(element, cls) {
   return (" " + element.className + " ").indexOf(" " + cls + " ") > -1;
@@ -10,8 +11,10 @@ navBtn.addEventListener("click", function () {
   if (hasClass(showMenu, "hide-menu")) {
     showMenu.classList.remove("hide-menu");
     body.style.paddingLeft = "272px";
+    chip.style.left = "250px";
   } else {
     showMenu.classList.add("hide-menu");
     body.style.paddingLeft = "96px";
+    chip.style.left = "72px";
   }
 });
